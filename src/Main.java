ddc12b079495cab11fc4edd71614be3b157d0b9a
Keitty <116025068+Kei-Degregorio-Davinci@users.noreply.github.com>
2022-11-24 22:43:10 -0300

    add: docs

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,6 @@ public class Main {
         Shape s1= new Circle("red", false, 5.5);
         System.out.println(s1);
 
-
-
         Shape s3 = new Rectangle("red",false, 1.0, 2.0);
         System.out.println(s3);
 
